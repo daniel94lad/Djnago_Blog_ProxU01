c4071f11ffa85ffae630afbf0776811ef693be88
Daniel Lopez <daniel94lad@gmail.com>
2020-06-13 21:25:11 -0500

    'StaticSolved'

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -135,7 +135,7 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
-STATIC_ROOT =  os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+STATIC_ROOT =  os.path.join(os.path.dirname(BASE_DIR), "static")
 MEDIA_URL = "/media/"
 MEDIA_ROOT =  os.path.join(BASE_DIR, "media_cdn")
 
